85f36f2a2efcb465674918f416f6c5acc491c6cc
bevin1984 <bevin1984@gmail.com>
2013-03-25 10:32:44 +0800

    Add YMGlobal

---
+++ teaPlaza/YMGlobal.h
@@ -0,0 +1,18 @@
+//
+//  YMGlobal.h
+//
+//  Copyright (c) 2013年 maimaicha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Constants.h"
+#import "AppDelegate.h"
+
+@interface YMGlobal : NSObject
+
+//获取MKNetworkOperation
++ (MKNetworkOperation *)getOperation:(NSMutableDictionary *)params;
+// 加载图片
++ (void)loadImage:(NSString *)imageUrl andImageView:(UIImageView *)imageView;
++ (void)loadImage:(NSString *)imageUrl andButton:(UIButton *)button andControlState:(UIControlState)buttonState;
+@end
---
+++ teaPlaza/YMGlobal.m
@@ -0,0 +1,30 @@
+//
+//  YMGlobal.m
+//  Copyright (c) 2013年 maimaicha. All rights reserved.
+//
+
+#import "YMGlobal.h"
+
+@implementation YMGlobal
+
++ (MKNetworkOperation *)getOperation:(NSMutableDictionary *)params
+{
+    [params setObject:API_KEY forKey:@"apikey"];
+    [params setObject:API_FORMAT forKey:@"format"];
+    return [ApplicationDelegate.appEngine operationWithPath:API_BASEURL params:params httpMethod:API_METHOD ssl:NO];
+}
+
++ (void)loadImage:(NSString *)imageUrl andImageView:(UIImageView *)imageView
+{
+    [ApplicationDelegate.appEngine imageAtURL:[NSURL URLWithString:imageUrl] onCompletion:^(UIImage *fetchedImage, NSURL *url, BOOL isInCache) {
+        [imageView setImage:fetchedImage];
+    }];
+}
+
++ (void)loadImage:(NSString *)imageUrl andButton:(UIButton *)button andControlState:(UIControlState)buttonState
+{
+    [ApplicationDelegate.appEngine imageAtURL:[NSURL URLWithString:imageUrl] onCompletion:^(UIImage *fetchedImage, NSURL *url, BOOL isInCache) {
+        [button setBackgroundImage:fetchedImage forState:buttonState];
+    }];
+}
+@end
