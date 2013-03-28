0ddc2814379706401b32648242fe15bc953c12a9
bevin1984 <bevin1984@gmail.com>
2013-03-28 14:59:58 +0800

    Add RegisterViewController

---
+++ teaPlaza/RegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.h
+//  teaPlaza
+//
+//  Created by bevin chen on 13-3-28.
+//  Copyright (c) 2013年 maimaicha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@end
